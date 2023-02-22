f971d947a83b90eecb6f8bfbcca542e0ea068e15
GunaRakulan-GR <gunarakulan@gmail.com>
2023-02-22 17:10:00 +0530

    chore: add roi, offset, get_center function AB#67

--- research/visitor-counter.py
+++ research/visitor-counter.py
@@ -3,9 +3,19 @@ import cv2
 import imutils
 
 
+ROI = 300
+offset = 8
 width_and_hieght = 1000
 
 cap = cv2.VideoCapture(0)
+
+def get_Center(x, y, w, h): 
+    x1 = int(w / 2)
+    y1 = int(h / 2)
+    cx = x + x1
+    cy = y + y1
+    return cx, cy
+
 
 if not cap.isOpened():
     print("ERROR: Failed to open webcam")
