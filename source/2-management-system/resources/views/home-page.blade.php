5f3882f8d93834669c51c9e043d68b1d2ae218dc
GunaRakulan-GR <gunarakulan@gmail.com>
2023-03-21 17:10:37 +0530

    feat(ms): build UI to filter by month in home-page.balde.php AB#115

--- source/2-management-system/resources/views/home-page.blade.php
+++ source/2-management-system/resources/views/home-page.blade.php
@@ -7,14 +7,27 @@
     <!-- Page Heading -->
 
     <div class="row">
+    
+        <div class="jumbotron col-xl-12 col-lg-12" style="background-clip: #f8f9fc;">
+            <div class="col-md-3 offset-md-3 col-lg-3 offset-lg-4">
+                <div class="input-group input-group-sm mb-3">
+                    <div class="input-group-prepend">
+                        <span style="font-weight: bold;" class="input-group-text" id="inputGroup-sizing-sm">Filter by Month:</span>
+                    </div>
+                    <input type="month" id="date_picker" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                </div>
+             </div>
+        </div>
 
         <!-- Area Chart -->
         <div class="col-xl-12 col-lg-12">
             <div class="card shadow mb-4">
+
+                
                 <!-- Card Header - Dropdown -->
                 <div
                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">February - 2023</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Customer Traffic Tracker</h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
